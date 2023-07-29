809d9a56e3737b6a56736de48ad0c04ec6dd3bf6
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2023-07-29 16:17:33 +0530

    fix:icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,9 +13,8 @@ export const Header = () => {
   return (
     <div className="bg-white py-4 flex items-center justify-around sticky top-0 z-20">
       <Link to="/" className="flex items-center gap-0.5 text-2xl">
-        <GrCirclePlay className="text-2xl text-primaryColor" />
-        <h1>
-          <span className="text-primaryColor">Crafty</span>Creations
+        <h1 className="text-primaryColor">
+          VideoVerse
         </h1>
       </Link>
       <div className="border rounded flex items-center px-1 gap-2">
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,7 +6,8 @@ import { MdWatchLater } from "react-icons/md";
 export const SideBar = () => {
   const activeStyle = {
     fontWeight: "bold",
-    backgroundColor: " #8553fa48",
+    backgroundColor: "#1B5299",
+    color:"#fff"
   };
   return (
     <aside className="sticky flex flex-col justify-between h-[90vh] top-0 left-0 overflow-y-none overflow-x-hidden items-center border-r border-primaryDisabled">
